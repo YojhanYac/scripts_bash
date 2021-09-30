c7cce63ce70f0d1c120e9b5cccfb8982ac141ec6
yojhanyac <yac@live.com.ar>
2021-09-30 17:38:41 -0300

    update

--- debian.sh
+++ debian.sh
@@ -6,5 +6,12 @@ sudo apt -y install apache2
 hostname -I
 sudo apt update
 sudo apt upgrade
+sudo apt install unzip
+sudo apt update
+sudo apt upgrade
 sudo apt install -y gnupg
+sudo apt update
+sudo apt upgrade
 sudo apt-get -y install mysql-community-server
+sudo apt update
+sudo apt upgrade
