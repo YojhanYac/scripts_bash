f3f26c3bc13119a3945bb75f0c44816d06643804
yojhanyac <yac@live.com.ar>
2021-09-30 23:13:27 -0300

    update

--- debian.sh
+++ debian.sh
@@ -3,31 +3,17 @@
 sudo apt update
 sudo apt upgrade
 sudo apt -y install apache2
-hostname -I
-sudo apt update
-sudo apt upgrade
-sudo apt install -y gnupg
-sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.19-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.19-1_all.deb
-sudo apt update
-sudo apt upgrade
-sudo apt remove -y mysql-apt-config
-sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.19-1_all.deb
-sudo apt install ./mysql-apt-config_0.8.19-1_all.deb
-sudo sed -i  "s/jessie/buster/g" /etc/apt/sources.list.d/mysql.list
-sudo apt update
-sudo apt install -y mysql-server
-sudo apt update
-sudo apt upgrade
-sudo apt-get -y install mysql-community-server
-sudo apt update
-sudo apt upgrade
+sudo apt install -y mariadb-server
+sudo mysql_secure_installation
 sudo mysql -u root -p'XMPLAY1300' < db_wordpress.sql
-sudo apt -y install php7.4
-sudo apt -y install php-mysql
+sudo apt install -y php-{xml,mysql,zip}
 sudo wget https://es.wordpress.org/latest-es_AR.tar.gz
 sudo tar xf latest-es_AR.tar.gz -C /var/www/html
 sudo mv wp-config.php /var/www/html/wordpress/
-sudo apt install -y vim
 sudo systemctl restart apache2
+sudo apt install -y curl
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
 hostname -I
