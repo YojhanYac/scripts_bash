6118ee5611b99c6a82e8a9ce7dd89f550db22209
yojhanyac <yac@live.com.ar>
2021-09-30 22:17:51 -0300

    update

--- debian.sh
+++ debian.sh
@@ -23,3 +23,11 @@ sudo apt-get -y install mysql-community-server
 sudo apt update
 sudo apt upgrade
 sudo mysql -u root -p'XMPLAY1300' < db_wordpress.sql
+sudo apt -y install php7.4
+sudo apt -y install php-mysql
+sudo wget https://es.wordpress.org/latest-es_AR.tar.gz
+sudo tar xf latest-es_ES.tar.gz -C /var/www/html
+sudo mv wp-config.php /var/www/html/wordpress
+sudo apt install -y vim
+sudo systemctl restart apache2
+hostname -I
